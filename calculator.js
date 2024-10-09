7541a3b0df75f2c54d977dd9918482e95c0130ee
leolice36 <leoliceanaelmojica@gmail.ccom>
2024-10-10 05:45:07 +0800

    Makes calculator work
    also adds fail safes that enable and
    disables buttons according to inteded sequence
    of use

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+//operation variables
+let num1;
+let num2;
+let opSymbol;
+
+//operation functions
 const add = function(a,b) {
     return a + b
 };
@@ -14,12 +20,6 @@ const divide = function(a,b) {
     return a / b
   };
 
-
-//operation variables
-let num1;
-let num2;
-let opSymbol;
-
 function operate(operator,firstNum,secondNum){
     if (operator === '+'){
         return add(firstNum,secondNum)
@@ -32,5 +32,108 @@ function operate(operator,firstNum,secondNum){
     }
     else if (operator === '/'){
         return divide(firstNum,secondNum)
+    } else {
+        return num1
     }
 }
+
+const display = document.querySelector('#display');
+
+//Enable/disable functions
+function enableAllBtn() {
+    let allBtn = document.querySelectorAll('button');
+    allBtn.forEach(btn => {
+        btn.classList.remove("disabled");
+    })
+}
+
+
+
+//Number button display function
+const numBtn = document.querySelectorAll('.numberBtn');
+
+numBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        display.textContent += button.textContent;
+        enableAllBtn();
+    })
+});
+
+
+//operation button functions
+
+const opBtn = document.querySelectorAll('.opBtn');
+opBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        console.table({opSymbol,num1,num2})
+        enableAllBtn(); 
+        if (!opSymbol){
+        opSymbol = button.dataset.value;
+        } 
+        
+        
+        if (!num1){
+            num1 = Number(display.textContent);
+            display.textContent = '';
+            
+        } else if (!num2){
+            num2 = Number(display.textContent);
+            display.textContent = '';
+            num1 = Number(operate(opSymbol,num1,num2))
+            num2 ='';
+            opSymbol = button.dataset.value;
+            
+        } else {
+            num1 = Number(display.textContent);
+            num2 ='';
+            display.textContent = '';
+            
+        }
+        console.table({opSymbol,num1,num2}) 
+        button.classList.add("disabled");
+    })
+});
+
+
+const equalBtn = document.querySelector('#equal');
+
+function equals(){
+    if (!num1){
+        num1 = Number(display.textContent);
+    } else {
+        num2 = Number(display.textContent);
+        display.textContent = operate(opSymbol,num1,num2)
+        num1 = Number(display.textContent);
+        opSymbol = '';
+    }
+    console.table({opSymbol,num1,num2}) 
+}
+
+equalBtn.addEventListener('click', () => {
+    enableAllBtn()
+    equals()
+    equalBtn.classList.add("disabled")
+    const numbersAnddeci = document.querySelectorAll('.numberBtn', '#del');
+    numbersAnddeci.forEach(button => {
+        button.classList.add("disabled")
+    })
+    
+})
+
+
+//other button functions
+const numDel = document.querySelector('#del');
+numDel.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0,-1)
+});
+
+const numAc = document.querySelector('#AC');
+numAc.addEventListener('click', () => {
+    display.textContent = '';
+    num1 = undefined;
+    num2 = undefined;
+    opSymbol = undefined;
+    console.table({opSymbol,num1,num2})
+    enableAllBtn()
+})
+
